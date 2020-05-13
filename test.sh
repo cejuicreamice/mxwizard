5f1ac7469d9f9c4a9a0c3e50f7c3f257ed4f2a2e
Jorge A. Flores <jorge.nunez@cimat.mx>
2020-05-13 11:07:49 -0500

    Include test suite 🧪.

--- test.sh
+++ test.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-# customize with your own.
-options=("AAA" "BBB" "CCC" "DDD")
+options=("Julia" "Anaconda" "NodeJS & GatsbyJs")
 
 menu() {
-    echo "Avaliable options:"
+    echo "Select options:"
     for i in ${!options[@]}; do 
-        printf "%3d%s) %s\n" $((i+1)) "${choices[i]:- }" "${options[i]}"
+        printf "[%s] %3d) %s\n" "${choices[i]:- }" $((i+1)) "${options[i]}"
     done
     if [[ "$msg" ]]; then echo "$msg"; fi
 }
 
-prompt="Check an option (again to uncheck, ENTER when done): "
+prompt="Check or uncheck an option (press <Enter> when done): "
 while menu && read -rp "$prompt" num && [[ "$num" ]]; do
     [[ "$num" != *[![:digit:]]* ]] &&
     (( num > 0 && num <= ${#options[@]} )) ||
     { msg="Invalid option: $num"; continue; }
-    ((num--)); msg="${options[num]} was ${choices[num]:+un}checked"
+    ((num--)); msg="${options[num]} has been ${choices[num]:+un}checked."
     [[ "${choices[num]}" ]] && choices[num]="" || choices[num]="+"
 done
 
